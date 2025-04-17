8d0d483a597f60cf9a8cbcb4e2295dea17c12533
Vincent Paul <90044187+VincentSamuelPaul@users.noreply.github.com>
2025-04-17 10:52:12 +0530

    first commit

---
+++ src/ui/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+
+  return (
+    <div className=''>
+      <h1>Hey jude</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
